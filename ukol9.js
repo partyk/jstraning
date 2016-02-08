5b28b6e219bf30907ada31e8230e222822b17cfe
Bohdan Partyk <info@podebrady.org>
2016-02-08 21:46:58 +0100

    ukol 9

--- ukol9.js
+++ ukol9.js
@@ -1,12 +1,12 @@
-var novyPrvej = function(element, css) {
+var novyPrvek = function(element, css) {
   var element = document.createElement(element);
-  css ? element.className = element.join(";") : "";
+  element.className = css.join(";");
   return function(text){
-   return  element.appendChild(document.createTextNode(text)).toString();
+   return element.appendChild(document.createTextNode(text)).toString();
   }
 }
 
 var mujPrvek = novyPrvek('div', ['color:red', 'width:300px']);
 
 console.log(mujPrvek('Ahoj, kamarade'));
-console.log(mujPrvek('Kocicka'))
+console.log(mujPrvek('Kocicka'));

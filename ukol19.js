91d9af5cc43ba669dbc48682ac47bf4c4748990e
Bohdan Partyk <Bohdan Partyk>
2016-03-05 21:23:22 +0100

    Ukol 19

---
+++ ukol19.js
@@ -0,0 +1,12 @@
+"use strict";
+switch(a) {
+    case (a === 1 && b < 5 && (x-y) === 20):
+        console.log('vysledek 1');
+        break;
+    case (c > 3 && d(e) === f):
+        console.log('vysledek 2');
+        break;
+    case (c > 3):
+         console.log('vysledek 3');
+        break;
+}
